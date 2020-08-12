a1436ab7a75f3e95a567dfe5074f7f7af73df470
Hiram Montesillo <montesillo25@gmail.com>
2020-08-11 21:38:47 -0500

    Variables y métodos estaticos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Anahí Salgado");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor.id++;
+
+        Doctor MyDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
     }
 }
