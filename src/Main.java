593d0f0d95fb73a89a47ba0aab10b7de5d89b398
Hiram Montesillo <montesillo25@gmail.com>
2020-08-24 21:27:46 -0500

    constantes

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,7 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodríguez";
-        myDoctor.showName();
-        myDoctor.showId();
 
-        Doctor.id++;
-
-        Doctor MyDoctorAnn = new Doctor();
-        myDoctor.showId();
 
         showMenu();
     }
--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -3,6 +3,7 @@ package ui;
 import java.util.Scanner;
 
 public class UIMenu {
+    public static String[] MONTHS = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opciÃ³n deseada");
@@ -49,6 +50,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 0; i < 3; i++){
+                        System.out.println(i + ". " + MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
